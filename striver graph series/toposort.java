89684fa3c91cb83b56192c57c8f6fdbac0e7c19d
vighnesh <vighnesh2607@gmail.com>
2023-04-08 18:05:39 +0530

    april 8 2023 third commit

---
+++ striver graph series/toposort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    
+    public static void helper(ArrayList<ArrayList<Integer>> adj, Stack<Integer> st, boolean[] visited,int i){
+        
+        visited[i]=true;
+        
+        for(Integer n:adj.get(i)){
+            if(!visited[n])
+            helper(adj,st,visited,n);
+        }
+        
+        st.push(i);
+    }
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        
+        Stack<Integer> st=new Stack<>();
+        int[] ans=new int[V];
+        
+        boolean[] visited=new boolean[V];
+        
+        for(int i=0;i<V;i++){
+            if(!visited[i])
+            helper(adj,st,visited,i);
+        }
+        
+        int temp=0;
+        while(st.size()>0){
+            ans[temp]=st.pop();
+            temp++;
+        }
+        
+        return ans;
+    }
+}
